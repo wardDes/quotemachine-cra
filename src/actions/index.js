0f32c5f3816c60d8792fa0bb4c3dd532907eb218
Daniel Howard <danielh9049@att.net>
2019-11-24 18:37:41 -0800

    restart with store, actions and action-types

--- src/actions/index.js
+++
@@ -1,5 +0,0 @@
-import { DATA_LOADED } from  '../constants/index.js'
-
-export function getData(payload){
-    return { type: DATA_LOADED, payload}
-}
--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,7 @@
-import { createStore, applyMiddleware, compose } from 'redux'
+import { createStore } from 'redux'
 import rootReducer from '../reducers/index.js'
-import thunk from 'redux-thunk'
 
-const storeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(
-    rootReducer,
-    storeEnhancers(applyMiddleware(thunk))
-)
+const store = createStore(rootReducer)
 
 export default store
