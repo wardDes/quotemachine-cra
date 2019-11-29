8b1751b40e946ff02d967b34689dd35719e9e5de
Daniel Howard <danielh9049@att.net>
2019-11-29 14:03:15 -0800

    remove /build from gitignore file

---
+++ build/precache-manifest.41f55a111d0c09e930a9c027c3c92cba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a314c895727a1caea9fe4422b904d313",
+    "url": "/walkThrough/index.html"
+  },
+  {
+    "revision": "43d96f61fe6b728c6876",
+    "url": "/walkThrough/static/css/main.281b7418.chunk.css"
+  },
+  {
+    "revision": "1a4e82cdfe869c949341",
+    "url": "/walkThrough/static/js/2.3f031f74.chunk.js"
+  },
+  {
+    "revision": "43d96f61fe6b728c6876",
+    "url": "/walkThrough/static/js/main.bf76c8f5.chunk.js"
+  },
+  {
+    "revision": "fa5b2a57ac07e0aac622",
+    "url": "/walkThrough/static/js/runtime-main.0ba0313a.js"
+  }
+]);
