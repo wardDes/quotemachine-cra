612278096bee74ff0be0c7d75c143eac18276cf1
Daniel Howard <danielh9049@att.net>
2019-11-28 16:43:23 -0800

    created constants for use in actions and reducers to implement redux

---
+++ src/constants/action-types.js
@@ -0,0 +1,10 @@
+
+export const CHANGE_QUOTE =  "CHANGE_QUOTE"
+export const CHANGE_COLOR = "CHANGE_COLOR"
+export const CHANGE_AUTHOR = "CHANGE_AUTHOR"
+
+export default {
+    CHANGE_COLOR,
+    CHANGE_QUOTE,
+    CHANGE_AUTHOR
+}
