65cbcb346d7a1d55200cdea8e392f9010d4c0274
Daniel Howard <danielh9049@att.net>
2019-11-28 16:47:15 -0800

    created reducer for each action

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,32 @@
-const intiialState = {
-    quotes: []
+import { CHANGE_COLOR, CHANGE_QUOTE, CHANGE_AUTHOR} from '../constants/action-types'
+
+/*
+Problem making reducers work when starting functions
+with 'state' equal to type of object I want the reducer to return.
+Tried to use "", empty string but code refused to compile
+*/
+export const quote = (state = null, action) => {
+    switch(action.type){
+        case CHANGE_QUOTE:
+            return action.quote
+        default: return state
+    }
 }
 
-function rootReducer(state=intiialState, action){
-    return state
+export const author = (state = null, action) => {
+    switch(action.type){
+        case CHANGE_AUTHOR:
+            return action.author
+        default: return state
+    }
 }
 
-export default rootReducer
+
+export const color = (state = null, action) => {
+    switch(action.type){
+        case CHANGE_COLOR:
+            return action.color
+        default: return state
+    }
+}
+
