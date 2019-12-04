1de974b19d7e68dd05068105cf26d2ea56b1dfcd
wardDes <dar-el@outlook.com>
2019-12-03 17:04:32 -0800

    created changequote, changeauthor, changecolor actions for reducers

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,25 +1,17 @@
-import C from '../constants/action-types'
+import {CHANGE_COLOR, CHANGE_QUOTE, CHANGE_AUTHOR} from '../constants/action-types'
 
 
+export const changeColor = (color) => ({
+    type: CHANGE_COLOR,
+    color
+})
 
-export const quoteAuthor = (state="", action) => {
-    switch(action.type){
-        case C.CHANGE_QUOTEOBJ:
-            return {
-                quote: action.quote,
-                author: action.author
-            }
-        default: return state
-    }
-}
+export const changeQuote = (quote) => ({
+    type: CHANGE_QUOTE,
+    quote
+})
 
-export const color = (state="", action) => {
-    switch(action.type){
-        case C.CHANGE_COLOR:
-            return {
-                color: action.color
-            }
-        default: return state
-    }
-}
-
+export const changeAuthor = (author) => ({
+    type: CHANGE_AUTHOR,
+    author
+})
