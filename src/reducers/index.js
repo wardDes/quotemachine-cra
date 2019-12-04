a9e65955b324b212683adb7e609bf237fc8aca56
wardDes <dar-el@outlook.com>
2019-12-03 17:06:41 -0800

    created quote, action and color reducers to return actions of appropriate type

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,28 @@
-const intiialState = {
-    quotes: []
+import { CHANGE_AUTHOR, CHANGE_QUOTE, CHANGE_COLOR} from '../constants/action-types'
+
+
+
+export const quote = (state=null, action) => {
+    switch(action.type){
+        case CHANGE_QUOTE:
+            return action.quote 
+        default: return state
+    }
 }
 
-function rootReducer(state=intiialState, action){
-    return state
+export const author = (state=null, action) => {
+    switch(action.type){
+        case CHANGE_AUTHOR:
+            return action.author
+        default: return state
+    }
 }
 
-export default rootReducer
+export const color = (state=null, action) => {
+    switch(action.type){
+        case CHANGE_COLOR:
+            return action.color
+        default: return state
+    }
+}
+
