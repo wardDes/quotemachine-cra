f4f2b14ef8d89f251ade2ca1c64afaab43c7e259
wardDes <dar-el@outlook.com>
2019-12-14 15:12:28 -0800

    removed quote and author reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,27 +1,20 @@
-import { CHANGE_AUTHOR, CHANGE_QUOTE, CHANGE_COLOR} from '../constants/action-types'
+import { CHANGE_COLOR, CHANGE_QUOTEOBJ} from '../constants/action-types'
 
 
 
-export const quote = (state=null, action) => {
-    switch(action.type){
-        case CHANGE_QUOTE:
-            return action.quote 
-        default: return state
-    }
-}
-
-export const author = (state=null, action) => {
-    switch(action.type){
-        case CHANGE_AUTHOR:
-            return action.author
-        default: return state
-    }
-}
 
 export const color = (state=null, action) => {
     switch(action.type){
         case CHANGE_COLOR:
             return action.color
+        default: return state
+    }
+}
+
+export const quoteObj = (state=null, action) => {
+    switch(action.type){
+        case CHANGE_QUOTEOBJ:
+            return action.quoteObj
         default: return state
     }
 }
